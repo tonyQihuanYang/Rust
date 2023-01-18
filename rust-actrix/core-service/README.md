8a793935d01f374b36cc7f424bd29df77d2f8c1d
QihuanY(Tony) <qihuany@fleetprofitcenter.com>
2023-01-18 08:59:09 -0600

    Build: Initialize Docker-file and compose

---
+++ rust-actrix/core-service/README.md
@@ -0,0 +1,8 @@
+```cmd
+docker build -t core-service .
+docker run -p 8080:8080 core-service
+
+
+docker build -t core-service . -f Dockerfile.dev
+
+```
--- rust-actrix/core-service/server/src/main.rs
+++ rust-actrix/core-service/server/src/main.rs
@@ -1,17 +1,37 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 mod controllers;
 use crate::controllers::authenticate_controller;
-use database::{run_migration, establish_connection};
+use database::{establish_connection, run_migration};
 
 #[get("/healthcheck")]
 async fn healthcheck() -> impl Responder {
     HttpResponse::Ok().body("ok")
 }
 
+use log::{Level, Metadata, Record};
+struct SimpleLogger;
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+    fn flush(&self) {}
+}
+
+use log::{LevelFilter, SetLoggerError};
+static LOGGER: SimpleLogger = SimpleLogger;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info));
 
-    let conn= &mut establish_connection();
+    log::info!("Started");
+    println!("wtf");
+    let conn = &mut establish_connection();
     run_migration(conn);
 
     HttpServer::new(|| {
